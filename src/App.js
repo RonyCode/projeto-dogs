3066854ca674ae20c1e9ac7c8f258109bbb65d65
ronycode <ronycode@outlook.com>
2021-03-25 02:33:32 -0300

    formlogin

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Header from "./Components/Layout/Header/Header";
 import Footer from "./Components/Layout/Footer/Footer";
 import Home from "./Components/Pages/Home/Home";
 import Login from "./Components/Pages/Login/Login";
-import "./App.css";
 import { UserStorage } from "./Hooks/UserContext";
+import "./App.css";
+import LoginPasswordLost from "./Components/Pages/Login/LoginPasswordLost";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
+            <Route path="/login/perdeu" element={<LoginPasswordLost />} />
           </Routes>
           <Footer />
         </UserStorage>
